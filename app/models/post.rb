b901b61d58dce88b415c1087b5f976e0ec8bfd31
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-06 18:00:24 -0600

    Images on each post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: { minimum: 5 }
   has_rich_text :body
+  has_many_attached :images
   after_create_commit { broadcast_prepend_to "posts" }
 end
